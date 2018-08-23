f8843e159affa9e743b053505434951d20243c62
Christina <clingc7@gmail.com>
2018-08-23 13:17:08 -0700

    add instructions and volume

--- main.js
+++ main.js
@@ -5,91 +5,109 @@ const sounds = {
   65: {
     char: 'A',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/kick1.mp3'),
+    vol: 1,
     name: 'KICK 1'
   },
   83: {
     char: 'S',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/kick3.mp3'),
+    vol: 1,
     name: 'KICK 2'
   },
   68: {
     char: 'D',
-    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/snare3.mp3'),
+    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/snareclap.mp3'),
+    vol: 0.6,
     name: 'SNARE 1'
   },
   70: {
     char: 'F',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/snare19.mp3'),
+    vol: 1,
     name: 'SNARE 2'
   },
   81: {
     char: 'Q',
-    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/clap4.mp3'),
+    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/9clap1.mp3'),
+    vol: 0.7,
     name: 'CLAP 1'
   },
   87: {
     char: 'W',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/clap10.mp3'),
+    vol: 1,
     name: 'CLAP 2'
   },
   69: {
     char: 'E',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/hat10.mp3'),
-    name: 'CLOSED HAT'
+    vol: 1,
+    name: 'HAT 1'
   },
   82: {
     char: 'R',
-    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/openhat.mp3'),
-    name: 'OPEN HAT'
+    audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/hat7.mp3'),
+    vol: 0.5,
+    name: 'HAT 2'
   },
   72: {
     char: 'H',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/pop1.mp3'),
+    vol: 1,
     name: 'PERC 1'
   },
   74: {
     char: 'J',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/synth3.mp3'),
+    vol: 1,
     name: 'SYNTH'
   },
   75: {
     char: 'K',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/strings2.mp3'),
+    vol: 1,
     name: 'STRINGS 1'
   },
   76: {
     char: 'L',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/strings22.mp3'),
+    vol: 1,
     name: 'STRINGS 2'
   },
   85: {
     char: 'U',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/wood1.mp3'),
+    vol: 1,
     name: 'PERC 2'
   },
   73: {
     char: 'I',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/harp1.mp3'),
+    vol: 1,
     name: 'HARP 1'
   },
   79: {
     char: 'O',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/harp2.mp3'),
+    vol: 1,
     name: 'HARP 2'
   },
   80: {
     char: 'P',
     audio: new Audio('https://mpc2000.nyc3.digitaloceanspaces.com/harp3.mp3'),
+    vol: 1,
     name: 'HARP 3'
   }
 }
 
 // Play sounds
 function playSound(code) {
-  const audio = sounds[code].audio;
-  audio.play();
-  audio.currentTime = 0;
-  display.innerText = sounds[code].name;
+  const sound = sounds[code].audio;
+  const text = sounds[code].name;
+  sound.play();
+  sound.volume = sounds[code].vol;
+  sound.currentTime = 0;
+  display.innerText = text;
 }
 
 // Trigger sounds
@@ -128,3 +146,18 @@ function pressBtn(e) {
 buttons.forEach(function(button) {
   button.addEventListener('click', pressBtn)
 })
+
+// Hover instructions
+pads.forEach(function(pad) {
+  addEventListener('mouseover', instructions)
+})
+
+pads.forEach(function(pad) {
+  addEventListener('click', instructions)
+})
+
+function instructions(e) {
+  if (e.target.classList.contains('pad')) {
+    display.innerText = "Press a key to begin!"
+  }
+}
